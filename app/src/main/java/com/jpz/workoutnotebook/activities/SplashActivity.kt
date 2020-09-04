1b33071d7230edae384d0732809c9234dfe14f6f
jeanpierreZ <jeanpierrezingraff@gmail.com>
2020-09-04 13:34:37 +0200

    Manage the authentication return.

--- app/src/main/java/com/jpz/workoutnotebook/activities/SplashActivity.kt
+++ app/src/main/java/com/jpz/workoutnotebook/activities/SplashActivity.kt
@@ -1,9 +1,16 @@
 package com.jpz.workoutnotebook.activities
 
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.ErrorCodes
+import com.firebase.ui.auth.IdpResponse
 import com.jpz.workoutnotebook.R
+import com.jpz.workoutnotebook.utils.MyUtils
+import kotlinx.android.synthetic.main.activity_splash.*
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
@@ -11,14 +18,51 @@ import kotlinx.coroutines.launch
 class SplashActivity : AppCompatActivity() {
 
     companion object {
-        // Identifier for Sign-In Activity
         const val RC_SIGN_IN: Int = 100
     }
 
-    // Choose authentication providers
+    private val myUtils = MyUtils()
+
+    // Authentication providers
     private val providers = arrayListOf(
         AuthUI.IdpConfig.EmailBuilder().build()
     )
+
+    @SuppressLint("SwitchIntDef")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RC_SIGN_IN) {
+            val response = IdpResponse.fromResultIntent(data)
+            if (resultCode == Activity.RESULT_OK) {
+                // Successfully signed in
+                myUtils.showSnackBar(
+                    splashActivityCoordinatorLayout,
+                    R.string.authentication_succeed
+                )
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                if (response == null) {
+                    myUtils.showSnackBar(
+                        splashActivityCoordinatorLayout,
+                        R.string.sign_in_cancelled
+                    )
+                } else {
+                    when (response.error?.errorCode) {
+                        ErrorCodes.NO_NETWORK -> myUtils.showSnackBar(
+                            splashActivityCoordinatorLayout,
+                            R.string.error_no_internet
+                        )
+                        ErrorCodes.UNKNOWN_ERROR -> myUtils.showSnackBar(
+                            splashActivityCoordinatorLayout,
+                            R.string.error_unknown_error
+                        )
+                    }
+                }
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/com/jpz/workoutnotebook/utils/MyUtils.kt
@@ -0,0 +1,12 @@
+package com.jpz.workoutnotebook.utils
+
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import com.google.android.material.snackbar.Snackbar
+
+class MyUtils {
+
+    fun showSnackBar(coordinatorLayout: CoordinatorLayout, text: Int) {
+        Snackbar.make(coordinatorLayout, text, Snackbar.LENGTH_SHORT).show()
+    }
+
+}
