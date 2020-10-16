670f455eee1955ddc993e97fd3c717ffdfcae88b
jeanpierreZ <jeanpierrezingraff@gmail.com>
2020-10-16 15:55:03 +0200

    Add Material-Calendar-View dependency. Add a calendar with a setOnDayClickListener and test it. Update other dependencies.

--- app/src/main/java/com/jpz/workoutnotebook/fragments/CalendarFragment.kt
+++ app/src/main/java/com/jpz/workoutnotebook/fragments/CalendarFragment.kt
@@ -4,8 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.applandeo.materialcalendarview.EventDay
+import com.applandeo.materialcalendarview.listeners.OnDayClickListener
 import com.jpz.workoutnotebook.R
+import kotlinx.android.synthetic.main.fragment_calendar.*
+import java.util.*
 
 
 class CalendarFragment : Fragment() {
@@ -17,4 +22,33 @@ class CalendarFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_calendar, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        calendarView.setOnDayClickListener(object : OnDayClickListener {
+            override fun onDayClick(eventDay: EventDay) {
+
+                val clickedDayCalendar: Calendar = eventDay.calendar
+                calendarView.setDate(clickedDayCalendar)
+
+                val clickedDayTime = clickedDayCalendar.time
+
+                Toast.makeText(activity, "clickedDayTime = $clickedDayTime", Toast.LENGTH_SHORT)
+                    .show()
+
+                addEvent(clickedDayCalendar)
+            }
+        })
+    }
+
+    //--------------------------------------------------------------------------------------
+
+    private fun addEvent(calendar: Calendar) {
+
+        val events: MutableList<EventDay> = ArrayList()
+        events.add(EventDay(calendar, R.drawable.ic_baseline_fitness_center_24))
+        calendarView.setEvents(events)
+    }
+
 }
