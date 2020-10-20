645cd8e38bf94e9244ffce9aa96ed8bbcd1ac2ce
jeanpierreZ <jeanpierrezingraff@gmail.com>
2020-10-20 15:54:47 +0200

    Add an AlertDialog to choose the workout for the training session and display it.

--- app/src/main/java/com/jpz/workoutnotebook/fragments/EditCalendarFragment.kt
+++ app/src/main/java/com/jpz/workoutnotebook/fragments/EditCalendarFragment.kt
@@ -1,22 +1,29 @@
 package com.jpz.workoutnotebook.fragments
 
+import android.app.Dialog
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import com.jpz.workoutnotebook.R
+import com.jpz.workoutnotebook.api.UserAuth
 import com.jpz.workoutnotebook.utils.DatePickerFragment
 import com.jpz.workoutnotebook.utils.DatePickerFragment.Companion.BUNDLE_KEY_DAY
 import com.jpz.workoutnotebook.utils.DatePickerFragment.Companion.BUNDLE_KEY_MONTH
 import com.jpz.workoutnotebook.utils.DatePickerFragment.Companion.BUNDLE_KEY_YEAR
 import com.jpz.workoutnotebook.utils.DatePickerFragment.Companion.REQUEST_KEY_DATE
+import com.jpz.workoutnotebook.utils.MyUtils
 import com.jpz.workoutnotebook.utils.TimePickerFragment
 import com.jpz.workoutnotebook.utils.TimePickerFragment.Companion.BUNDLE_KEY_HOUR
 import com.jpz.workoutnotebook.utils.TimePickerFragment.Companion.BUNDLE_KEY_MINUTE
 import com.jpz.workoutnotebook.utils.TimePickerFragment.Companion.REQUEST_KEY_TIME
+import com.jpz.workoutnotebook.viewmodels.WorkoutViewModel
 import kotlinx.android.synthetic.main.fragment_edit_calendar.*
+import org.koin.android.ext.android.inject
+import org.koin.androidx.viewmodel.ext.android.viewModel
 import java.text.DateFormat
 import java.util.*
 
@@ -25,20 +32,30 @@ class EditCalendarFragment : Fragment(), View.OnClickListener {
 
     companion object {
         private val TAG = EditCalendarFragment::class.java.simpleName
+        private const val WORKOUT_NAME_FIELD = "workoutName"
     }
 
     private val calendar = Calendar.getInstance()
+
+    private var userId: String? = null
+
+    private var allWorkoutName = mutableListOf<CharSequence>()
+
+    // Firebase Auth, Firestore and utils
+    private val userAuth: UserAuth by inject()
+    private val workoutViewModel: WorkoutViewModel by viewModel()
+    private val myUtils: MyUtils by inject()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         // Use the Kotlin extension in the fragment-ktx artifact for setFragmentResultListener
         // Listen the result from DatePickerFragment
-        childFragmentManager.setFragmentResultListener(REQUEST_KEY_DATE, this) { key, bundle ->
+        childFragmentManager.setFragmentResultListener(REQUEST_KEY_DATE, this) { _, bundle ->
             editCalendarFragmentDate.text = setCalendarDate(bundle)
         }
-
-        childFragmentManager.setFragmentResultListener(REQUEST_KEY_TIME, this) { key, bundle ->
+        // Listen the result from TimePickerFragment
+        childFragmentManager.setFragmentResultListener(REQUEST_KEY_TIME, this) { _, bundle ->
             editCalendarFragmentTime.text = setCalendarTime(bundle)
         }
     }
@@ -52,6 +69,8 @@ class EditCalendarFragment : Fragment(), View.OnClickListener {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        userId = userAuth.getCurrentUser()?.uid
 
         editCalendarFragmentButtonWorkout.setOnClickListener(this)
         editCalendarFragmentButtonDate.setOnClickListener(this)
@@ -88,9 +107,57 @@ class EditCalendarFragment : Fragment(), View.OnClickListener {
 
     //--------------------------------------------------------------------------------------
 
+    // Get the list of all workouts
+    private fun getAllWorkouts(allExerciseName: MutableList<CharSequence>) {
+        // Clear the list before use it
+        allWorkoutName.clear()
+
+        userId?.let { it ->
+            // Get the workouts from Firestore Query
+            workoutViewModel.getOrderedListOfExercises(it)?.get()
+                ?.addOnSuccessListener { documents ->
+                    if (documents.isEmpty) {
+                        myUtils.showSnackBar(
+                            editCalendarFragmentCoordinatorLayout, R.string.no_workout
+                        )
+                    } else {
+                        for (document in documents) {
+                            Log.d(TAG, "${document.id} => ${document.data}")
+                            // Add the workouts name to the list
+                            allWorkoutName.add(document.get(WORKOUT_NAME_FIELD) as CharSequence)
+                        }
+                        // Then show the AlertDialog with the list of workouts
+                        addAWorkoutAlertDialog(allExerciseName.toTypedArray())
+                    }
+                }
+                ?.addOnFailureListener { exception ->
+                    Log.w(TAG, "Error getting documents: ", exception)
+                }
+        }
+    }
+
+    // Display the AlertDialog with the list of all the workouts
+    private fun addAWorkoutAlertDialog(list: Array<CharSequence>): Dialog {
+        return activity?.let {
+            val builder = AlertDialog.Builder(it)
+            builder.setTitle(getString(R.string.add_a_workout))
+                .setNeutralButton(android.R.string.cancel) { dialog, _ ->
+                    dialog.dismiss()
+                }
+                .setItems(list) { _, which ->
+                    // Display the workout chosen
+                    editCalendarFragmentWorkout.text = list[which]
+                }
+                .create()
+            builder.show()
+        } ?: throw IllegalStateException("Activity cannot be null")
+    }
+
+    //--------------------------------------------------------------------------------------
+
     override fun onClick(v: View?) {
         when (v?.id) {
-            R.id.editCalendarFragmentButtonWorkout -> TODO()
+            R.id.editCalendarFragmentButtonWorkout -> getAllWorkouts(allWorkoutName)
             R.id.editCalendarFragmentButtonDate -> {
                 val datePicker = DatePickerFragment()
                 datePicker.show(childFragmentManager, DatePickerFragment()::class.java.simpleName)
